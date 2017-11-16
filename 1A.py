841a2dd2bfea443d426cf9fee2ba33e12b15b3c6
Manuel_Lee <Manuel_Lee>
2017-11-16 02:16:31 -1200

    first commit

---
+++ 1A.py
@@ -0,0 +1,8 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:\Users\HP\Desktop\emmanuel,py.py ================
+please enter value for radius:  12
+please enter value for angle in degree:  45
+('the result is ', 3.4641016151377544)
+>>> 
